ba1d56a777a3f7ed4318d1c56badbf891a9ff39e
zohaibadnan137 <zohaibadnan137@gmail.com>
2023-07-06 13:56:02 +0500

    🛠 config: Add deployment configuration for Sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,15 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.METAMASK_SEPOLIA_API_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
